266656af44b1558f3c1089753330b5c0e9a0c366
Gustavo de Camargo Campos <contato.wanjalagus@oulook.com.br>
2022-04-20 13:41:14 -0400

    moved view urls to recipes.views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return HttpResponse('Home 1')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
